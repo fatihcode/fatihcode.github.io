df189175190e964a80665703c2b700ff38884126
fatihQ <qaz3400@outlook.com>
2022-05-03 21:37:16 +0300

    data json added

---
+++ data.js
@@ -0,0 +1,113 @@
+export const project = [
+   {
+      id: 1,
+      name: "Quiz Application",
+      description: "A quiz application that allows users to take a quiz and get a score.",
+      tag: ["CSS", "Javascript", "Bootstrap"],
+      image: "quiz.png",
+      live: "https://fatihqaz.github.io/quiz/"
+   },
+   {
+      id: 2,
+      name: "Random Background",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia odio modi sunt reiciendis iusto nam quaerat voluptas consequuntur mollitia voluptates beatae ratione eligendi illo, iure veritatis!",
+      tag: ["CSS", "Javascript"],
+      image: "random.jpg",
+      live: "https://fatihqaz.github.io/random-color/"
+   },
+   {
+      id: 3,
+      name: "Red Ball",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia odio modi sunt reiciendis iusto nam quaerat voluptas consequuntur mollitia voluptates beatae ratione eligendi illo, iure veritatis!",
+      tag: ["CSS", "Javascript"],
+      image: "redball.jpg",
+      live: "https://fatihqaz.github.io/red-ball/"
+   },
+   {
+      id: 4,
+      name: "Dice Game",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia odio modi sunt reiciendis iusto nam quaerat voluptas consequuntur mollitia voluptates beatae ratione eligendi illo, iure veritatis!",
+      tag: ["CSS", "Javascript"],
+      image: "dice.jpg",
+      live: "https://fatihqaz.github.io/dice-challenge/"
+   },
+   {
+      id: 5,
+      name: "Guess Number",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia odio modi sunt reiciendis iusto nam quaerat voluptas consequuntur mollitia voluptates beatae ratione eligendi illo, iure veritatis!",
+      tag: ["CSS", "Javascript"],
+      image: "guess.jpg",
+      live: "https://fatihqaz.github.io/quess-number/"
+   },
+   {
+      id: 6,
+      name: "Bill Calculator",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia odio modi sunt reiciendis iusto nam quaerat voluptas consequuntur mollitia voluptates beatae ratione eligendi illo, iure veritatis!",
+      tag: ["CSS", "Javascript"],
+      image: "fatura.jpg",
+      live: "https://fatihqaz.github.io/fatura/"
+   },
+   {
+      id: 7,
+      name: "Exchange",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia odio modi sunt reiciendis iusto nam quaerat voluptas consequuntur mollitia voluptates beatae ratione eligendi illo, iure veritatis!",
+      tag: ["API", "Javascript"],
+      image: "doviz.jpg",
+      live: "https://fatihqaz.github.io/doviz/"
+   },
+   {
+      id: 8,
+      name: "Movies",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia odio modi sunt reiciendis iusto nam quaerat voluptas consequuntur mollitia voluptates beatae ratione eligendi illo, iure veritatis!",
+      tag: ["API", "Javascript", "Bootstrap"],
+      image: "movie.jpg",
+      live: "https://fatihqaz.github.io/movies/"
+   },
+   {
+      id: 9,
+      name: "React Phone Book",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia odio modi sunt reiciendis iusto nam quaerat voluptas consequuntur mollitia voluptates beatae ratione eligendi illo, iure veritatis!",
+      tag: ["React", "Javascript", "Bootstrap"],
+      image: "phone-book.jpg",
+      live: "https://fatihqaz.github.io/phone-book/"
+   },
+   {
+      id: 10,
+      name: "Todo App",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia odio modi sunt reiciendis iusto nam quaerat voluptas consequuntur mollitia voluptates beatae ratione eligendi illo, iure veritatis!",
+      tag: ["React", "Javascript", "CSS"],
+      image: "todo.jpg",
+      live: "https://fatihqaz.github.io/todo-list/"
+   },
+   {
+      id: 11,
+      name: "React Redux Human Resource",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia odio modi sunt reiciendis iusto nam quaerat voluptas consequuntur mollitia voluptates beatae ratione eligendi illo, iure veritatis!",
+      tag: ["Node.Js", "MongoDB", "API", "React", "Redux", "Javascript", "Bootstrap"],
+      image: "human.jpg",
+      live: "https://fatihqaz.github.io/human-resource/"
+   }
+]
+
+export const social = [
+   {
+      id: 1,
+      link: "qaz3400@outlook.com",
+      icon: "bi bi-envelope",
+   },
+   {
+      id: 2,
+      link: "https://twitter.com/fatih_js",
+      icon: "bi bi-twitter",
+   },
+   {
+      id: 3,
+      link: "https://github.com/fatihqaz",
+      icon: "bi bi-github",
+   },
+   {
+      id: 4,
+      link: "https://www.linkedin.com/in/fatih-q-749415230/",
+      icon: "bi bi-linkedin",
+   }
+]
