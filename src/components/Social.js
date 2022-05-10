fe09befd652dfb71e9c2af6131fa5df2d87d469c
fatihQ <qaz3400@outlook.com>
2022-05-10 14:58:38 +0300

    link update

--- src/components/Social.js
+++ src/components/Social.js
@@ -5,7 +5,7 @@ export default function Social() {
    return (
       <span className="fs-4 ">
          {social.map(item => (
-            <a key={item.id} href={item.link} title={item.title}  target="_blank" rel="noopener noreferrer">{item.icon}</a>
+            <a key={item.id} href={(item.title == "E-Mail" ? "mailto:" : "") + item.link} title={item.title} target="_blank" rel="noopener noreferrer">{item.icon}</a>
          ))}
       </span>
    )
