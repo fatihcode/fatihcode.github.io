aded62ee481396e8f7b1b200b9300b2242449f68
fatihQ <qaz3400@outlook.com>
2022-10-26 14:18:05 +0300

    chor: try

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './components/App';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
